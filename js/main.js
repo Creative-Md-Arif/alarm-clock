cf055120bff1da0d45298f92b853ab7562e465b0
Md.Arif <mohammadarif.ma02@gmail.com>
2024-03-12 08:58:09 -0700

    Update-main-js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-const selectMenu = document.querySelectorAll("select");
+const currentTime = document.querySelector("h1"),
+selectMenu = document.querySelectorAll("select");
 
 for (let i = 12; i > 0; i--) {
   i = i < 10 ? "0" + i : i;
@@ -19,6 +20,33 @@ for (let i = 2; i > 0; i--) {
    let option = `<option value="${ampm}">${ampm}</option>`;
    selectMenu[2].firstElementChild.insertAdjacentHTML("afterend", option);
 }
+
+
+setInterval(()=>{
+   // getting hour , mins, secs 
+
+   let date = new Date (),
+   h = date.getHours(),
+   m = date.getMinutes(),
+   s = date.getSeconds(),
+   ampm = "AM";
+
+if( h >= 12) {
+   h = h - 12;
+   ampm = "PM";
+}
+// if hour value is 0, set this value to 12
+h= h == 0 ? h = 12 : h ;
+
+// adding 0 before hr, min ,sec if this value is less than 10
+ h = h < 10 ? "0" + h : h;
+ m = m < 10 ? "0" + m : m;
+ s = s < 10 ? "0" + s : s;
+
+currentTime.innerText = `${h}:${m}:${s} ${ampm}`;
+
+
+}, 1000);
 
 
 
